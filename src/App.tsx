2f690978b987bcf99d0b350f9fd006c9db6da7e5
Quezka <arsdom15@outlook.com>
2025-09-05 07:55:33 +0200

    04/09/2025 added avatar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,35 @@ import "./App.css";
 import Button from "./components/Button/Button";
 import Loader from "./components/Loader/Loader";
 import Link from "./components/Link/Link";
+import Avatar from "./components/Avatar/Avatar";
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    alert("Button clicked!");
-    console.log("Button clicked!");
-  };
+	const handleClick = () => {
+		alert("Button clicked!");
+		console.log("Button clicked!");
+	};
 
-  return (
-    <>
-      <Button size="large" color="secondary" style="contained" isLoading>
-        Contained Small
-      </Button>
-      <br></br>
-      <br></br>
-      <Button size="large" color="secondary" style="outlined" isLoading>
-        Outlined Medium
-      </Button>
-      <br></br>
-      <br></br>
-      <Button size="large" color="secondary" style="text" isLoading>
-        Text Large
-      </Button>
-      <Link href="https://facebook.com/">Link</Link>
-    </>
-  );
+	return (
+		<>
+			<Button size="large" color="secondary" style="contained" isLoading>
+				Contained Small
+			</Button>
+			<br></br>
+			<br></br>
+			<Button size="large" color="secondary" style="outlined" isLoading>
+				Outlined Medium
+			</Button>
+			<br></br>
+			<br></br>
+			<Button size="large" color="secondary" style="text" isLoading>
+				Text Large
+			</Button>
+			<Link href="https://facebook.com/">Link</Link>
+			<Avatar size="small" img="" username="Arseniy Domashenko" isOnline></Avatar>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Avatar.scss";
+
+interface AvatarProps {
+	isOnline?: boolean;
+	shape?: "circ" | "rounded" | "square";
+	size: "large" | "big" | "medium" | "small";
+	img?: string;
+	username?: string;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ isOnline = false, shape = "circ", size, img, username }) => {
+	const [initials, setInitials] = useState("");
+
+	useEffect(() => {
+		if (username && username.split(" ").length == 2) {
+			setInitials(username?.split(" ")[0][0] + username?.split(" ")[1][0]);
+		}
+	});
+	return (
+		<div className={`avatar ${shape} ${size} online=${isOnline}`}>
+			{img && <img src={img} alt="User Avatar"></img>}
+			{username && <span className="username">{initials}</span>}
+			{isOnline && <div className="online-badge"></div>}
+			{!username && !img && (
+				<svg className="avatar-icon" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+					<path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z" fill="#000000" />
+					<path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" fill="#000000" />
+				</svg>
+			)}
+		</div>
+	);
+};
+
+export default Avatar;
